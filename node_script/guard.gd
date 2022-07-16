6cbaa52e910ae3e1cf5f7fa59b62343b5a657ec9
loteque <andrewandroid@gmail.com>
2022-07-16 13:04:45 +0200

    actually set z's to -z's, add some test code, it works.

--- node_script/guard.gd
+++ node_script/guard.gd
@@ -2,5 +2,23 @@ extends KinematicBody
 
 onready var door_guard_data = load("res://data/actor/guard.tres")
 
-func _ready() -> void:
-	pass
+var target
+
+func _process(delta: float) -> void:
+	if target:
+		look_at(target.global_transform.origin, Vector3.UP)
+
+func _on_Area_body_entered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		target = body
+		print(body.name + " entered")
+		set_color(Color(1, 0, 0))
+	
+func _on_Area_body_exited(body: Node) -> void:
+	if body.is_in_group("Player"):
+		target = null
+		print(body.name + " exited")
+		set_color(Color(0, 1, 0))
+
+func set_color(color):
+	$MeshInstance.get_surface_material(0).set_albedo(color)
