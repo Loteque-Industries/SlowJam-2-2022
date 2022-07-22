3985bb6c31555564e7a4104ee18ff7969d3fc359
loteque <andrewandroid@gmail.com>
2022-07-22 02:16:52 +0200

    update level changer, closes #114

--- node_script/level_changer.gd
+++ node_script/level_changer.gd
@@ -1,7 +1,10 @@
 extends Spatial
 
 func _on_LevelChangerBody_body_entered(body: Node) -> void:
+
 	if body.is_in_group("Player"):
-		print("res://scenes/level/Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
-		get_tree().change_scene("res://scenes/level/Level_" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
-		#get_tree().change_scene("res://scenes/level/Level_1.tscn")
+		var levels = range(1,9)
+		for i in range(1,11):
+			var level = ("res://scenes/level/Level_" + str(int(get_tree().current_scene.name) + i) + ".tscn")
+			print(level)
+			get_tree().change_scene(level)
